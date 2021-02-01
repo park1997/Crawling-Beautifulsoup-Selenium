802fa6ad5ffd2b5b4e98a691e6e9f943875a8d36
park1997 <iambyunghyun97@gmail.com>
2021-02-01 19:15:32 +0900

    Update 3_requests.py

--- Crawling/webscraping_basic/3_requests.py
+++ Crawling/webscraping_basic/3_requests.py
@@ -8,7 +8,7 @@ print("응답 코드 2: ",res1.status_code)
 if res.status_code==200:
     print("정상입니다.")
 else:
-    print("문제가 생겼스비다 . 에러코드 [",res1.status_code,"]")
+    print("문제가 생겼습니다 . 에러코드 [",res1.status_code,"]")
 
 #if 문을 쓰지 않고도 raise_for_status로 에러인지 확인가능 하다.
 res.raise_for_status()
